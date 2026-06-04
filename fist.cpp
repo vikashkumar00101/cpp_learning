f0cd90c9dd87695fe5ec4b9c1b3066fe8dbac93d
vikashkumar00101 <vkflarex@gmail.com>
2026-06-04 19:00:18 +0530

    Add files via upload

---
+++ fist.cpp
@@ -0,0 +1,426 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+//int a,b;
+//cout<<"enter your 1 no";
+//cin>>a;
+//cout<<"enter your 2 no";
+//cin>>b;
+//cout<<a*b;
+
+
+//int a,b;
+//cout<<"enter your first no";
+//cin>>a;
+//cout<<"enter your second no";
+//cin>>b;
+//if (a/b)
+//{
+  //  cout<<"yes";}
+    //else
+//{ 
+  //  cout<<"no";
+
+//}
+//int a;
+//cout<<"enter your no";
+//cin>>a;
+//cout<<a*a;
+
+
+//int a,b;
+
+//cout<<"enter your firsty no";
+//cin>>a;
+//cout<<"enter your second no";
+//cin>>b;
+//if (a/b>1)
+//{
+  //  cout<<a,b;
+
+//}
+//else
+//{
+ //if (a/b>1)
+ //{
+   /// cout<<a;
+ //}
+ //else
+ //{
+   // cout<<b;
+ //}
+//#include <iostream>
+//using namespace std;
+
+//int main() {
+   // int n;
+    // long long factorial = 1;
+
+    // cout << "Enter a number: ";
+    // cin >> n;
+
+    // if (n < 0) {
+    //     cout << "Factorial is not defined for negative numbers." << endl;
+    // } else {
+    //     for (int i = 1; i <= n; i++) {
+    //         factorial *= i;
+    //     }
+    //     cout << "Factorial of " << n << " is " << factorial << endl;
+    // }
+
+    // return 0;
+
+// int i,n;
+// cout<<"enter your fisrt no";
+// cin>>i;
+// cout<<"enter your secnd no";
+// cin>>n;
+// cout<<i*n;
+
+// int i,n;
+// cout<<"enter 1st no";
+// cin>>i;
+// cout<<"enter 2nd no";
+// cin>>n;
+// cout<<i-n;
+
+// int i,n;
+// cout<<"enter your 1st no";
+// cin>>i;
+// cout<<"enter your 2nd no";
+// cin>>n;
+
+
+// cout<<"6*3";
+
+// int i,n;
+// cout<<"enter your 1st no";
+// cin>>i;
+// cout<<"enter your second no";
+// cin>>n;
+// if (i>n)
+// {
+//     cout<<i;
+// }
+// else
+// {
+//     cout<<n;
+// }
+
+// int i;
+// cout<<"enter your age";
+// cin>>i;
+// if (i>=18)
+// {
+//     cout<<"adult";
+// }
+// else
+// {
+//     cout<<"teenager";
+// }
+
+// int i;
+// cout<<"enter your no";
+// cin>>i;
+// if (i==1)
+// {
+//     cout<<"january";
+// }
+// else if (i==2)
+// {
+// cout<<"febuary";
+// }
+// else if (i==3)
+// {
+//    cout<<"march";
+// }
+// else if (i==4)
+// {
+//     cout<<"april";
+// }
+// else if (i==5)
+// {
+//    cout<<"may";
+// }
+// else if (i==6)
+// {
+//    cout<<"june";
+
+// }
+// else if (i==7)
+// {
+//     cout<<"july";}
+//     else if (i==8)
+//     {
+//        cout<<"april";}
+//        else if (i==9)
+//        {
+//       cout<<"september";}
+//       else if (i==10)
+//       {
+//        cout<<"october";}
+//        else if (i==11)
+//        {
+//         cout<<"november";}
+//         else if (i==12)
+//         {
+//            cout<<"december";}
+        
+        
+    //   int i;
+    //   cout<<"enter your age";
+    //   cin>>i;
+    //   if (i<12)
+    //   {
+    //     cout<<"yes";
+    //   }
+    //    else if (i>65)
+    //    {
+    //     cout<<"yes";
+    //    }
+    //    else
+    //    {
+    //     cout<<"no";
+    //    }
+
+
+// for (int count =1; count<=20; count=count+1)
+// {
+//    cout<<"jai hind\n";
+// }
+
+
+// int n;
+// cout<<"enter your no";
+// cin>>n;
+// for (int i=1; i<=n;i=i+2)
+// {
+//     cout<<i<<endl;
+// }
+
+
+// int i;
+// cout<<"enter your no";
+// cin>>i;
+// for (int n=4;n<=i;n=n+4)
+// {
+//    cout<<n<<endl;
+// }
+
+// for (int i=280;i>=250;i=i-1)
+// {
+//   cout<<i<<" ";
+// }
+
+// char name='a';
+// for (char name='a';name<='z';name=name+1)
+// {
+//   cout<<name<<" ";
+// }
+
+// for (char name='z';name>='a';name=name-1)
+// {
+//  cout<<name<<" ";
+// }
+
+
+// int i;
+// for (i=220;i<=730;i=i+7)
+// {
+//   cout<<i<<" ";
+// }
+
+// int i,n,sum;
+
+// cout<<"enter your 1st no: ";
+// cin>>n;
+// sum=0;
+// for (i=1;i<=n;i=i+1)
+// {
+//  sum=sum+i*i;
+// }
+// {cout<<sum;}
+
+
+// int i,n,sum;
+// cout<<"enter your no: ";
+// cin>>n;
+// sum=0;
+// for (i=1;i<=n;i=i+1)
+// {
+//  sum=sum+i*i*i;
+// }
+// {cout<<sum;}
+
+
+
+//     int n;
+//     cout << "Enter position: ";
+//     cin >> n;
+
+//     int a = 0, b = 1, c;
+
+//     if(n == 1) {
+//         cout << a;
+//     }
+//     else if(n == 2) {
+//         cout << b;
+//     }
+//     else {
+//         for(int i = 3; i <= n; i++) {
+//             c = a + b;
+//             a = b;
+//             b = c;
+//         }
+
+//         cout << b;
+//     }
+
+//     return 0;
+// }
+
+
+
+//int row,col;
+// for (row=1;row<=6;row=row+1) 
+//  {
+//   for (col=1;col<=5;col=col+1) 
+//   {
+//     cout<<"4"<<" ";
+//   }
+//   cout<<endl;
+//  }
+
+
+//  int row,col,count;
+//  count=1;
+// for (row=1;row<=6;row=row+1)
+// {
+//  for (col=1;col<=5;col=col+1)
+//  {
+// cout<<col*col<<" ";
+//  }
+//  cout<<endl;
+// }
+
+
+// int row,col;
+// for (row=1;row<=5;row=row+1) 
+// {
+//  for (col=1;col<=6;col=col+1)
+//   {
+//     cout<<col*col*col<<" ";
+
+//  }
+//  cout<<endl;
+// }
+
+
+// int row,col;
+// char name='F';
+// for (row=1;row<=5;row=row+1) 
+// {
+//  for (col=1;col<=5;col=col+1) 
+//  {
+//  for (name='F';name<='F'+5;name=name+1) 
+//  {
+//   cout<<name<<" ";
+//  }
+//  cout<<endl;
+//  }
+//  return 0;
+// }
+
+// int row,col;
+// for (row=1;row<=6;row=row+1)
+// {
+//   for (col=1;col<=row;col=col+1) 
+//   {
+//     cout<<col<<" ";
+
+//   }
+//   cout<<endl;
+  
+// }
+  
+  
+
+// int row,col;
+// char name='A';
+// for (row=1;row<=5;row=row+1) 
+ 
+//   {
+//     for (name='A';name<='A'+row-1;name=name+1) 
+//     {
+//       cout<<name<<" ";
+//     }
+//     cout<<endl;
+//   }
+  
+
+// int row,i;
+
+// for (row=1;row<=6;row=row+1) 
+// {
+//   for (i=10;i<=10+row-1;i=i+1) 
+//   {
+//    cout<<i<<" ";
+//   }
+//   cout<<endl;
+// }
+
+
+// int row,i;
+// char name='A';
+// for (row=4;row>=1;row=row-1) 
+// {
+//   for (name='A';name<='A'+row-1;name=name+1)
+//   {
+    
+
+//     cout<<name<<" ";
+//   }
+//   cout<<endl;
+// }
+
+
+int row,col;
+for (row=1;row<=5;row=row+1)
+{
+for (col=1;col>=5-row;col=col+1) 
+{
+  cout<<" "<<" ";
+}
+cout<<endl;
+for (row=1;row<=5;row=row+1) 
+{
+  for (col=5;col<=row;col=col+1) 
+  {
+   cout<<row;
+  }
+  cout<<endl;
+}
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
